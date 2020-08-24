5ae7526519cee1ba329f4721b30f183e2853cca7
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-08-24 09:50:23 +0200

    solved

--- isolate/02-rendering-data/exercises/2-info.js
+++ isolate/02-rendering-data/exercises/2-info.js
@@ -10,149 +10,149 @@ const expect = chai.expect;
  * @returns {HTMLDetailsElement}
  */
 const renderInfo = (caption, mainText, id = '') => {
-  const detailsEl = document.createElement('_');
-  _;
+    const detailsEl = document.createElement('details');
+    detailsEl.id = id;
 
-  const summaryEl = document.createElement('_');
-  _;
-  _;
+    const summaryEl = document.createElement('summary');
+    summaryEl.innerHTML = caption;
+    detailsEl.appendChild(summaryEl);
 
-  const pEl = document.createElement('_');
-  _;
-  _;
+    const pEl = document.createElement('P');
+    pEl.innerHTML = mainText;
+    detailsEl.appendChild(pEl);
 
-  return detailsEl;
+    return detailsEl;
 };
 
 describe('renderInfo: renders some info under a caption', () => {
 
-  describe('several colors, no id', () => {
-    const actual = renderInfo('several colors', 'red, orange, green, blue.');
+    describe('several colors, no id', () => {
+        const actual = renderInfo('several colors', 'red, orange, green, blue.');
 
-    it('has tagName: "DETAILS"', () => {
-      expect(actual).to.have.property('tagName', 'DETAILS');
-    });
-    it('has id: ""', () => {
-      expect(actual).to.have.property('id', '');
-    });
-    it('has childElementCount: 2', () => {
-      expect(actual).to.have.property('childElementCount', 2);
-    });
-    console.dir(actual);
+        it('has tagName: "DETAILS"', () => {
+            expect(actual).to.have.property('tagName', 'DETAILS');
+        });
+        it('has id: ""', () => {
+            expect(actual).to.have.property('id', '');
+        });
+        it('has childElementCount: 2', () => {
+            expect(actual).to.have.property('childElementCount', 2);
+        });
+        console.dir(actual);
 
-    describe(`.children[0]`, () => {
-      const actualChild = actual.children[0];
-      it('has tagName: "SUMMARY"', () => {
-        expect(actualChild).to.have.property('tagName', 'SUMMARY');
-      });
-      it('has text: "several colors"', () => {
-        expect(actualChild).to.have.text('several colors');
-      });
-      it('has childElementCount: 0', () => {
-        expect(actualChild).to.have.property('childElementCount', 0);
-      });
-      console.dir(actualChild);
-    });
+        describe(`.children[0]`, () => {
+            const actualChild = actual.children[0];
+            it('has tagName: "SUMMARY"', () => {
+                expect(actualChild).to.have.property('tagName', 'SUMMARY');
+            });
+            it('has text: "several colors"', () => {
+                expect(actualChild).to.have.text('several colors');
+            });
+            it('has childElementCount: 0', () => {
+                expect(actualChild).to.have.property('childElementCount', 0);
+            });
+            console.dir(actualChild);
+        });
 
-    describe(`.children[1]`, () => {
-      const actualChild = actual.children[1];
-      it('has tagName: "P"', () => {
-        expect(actualChild).to.have.property('tagName', 'P');
-      });
-      it('has text: "red, orange, green, blue."', () => {
-        expect(actualChild).to.have.text('red, orange, green, blue.');
-      });
-      it('has childElementCount: 0', () => {
-        expect(actualChild).to.have.property('childElementCount', 0);
-      });
-      console.dir(actualChild);
-    });
-  });
-
-  describe('several colors, id "colors"', () => {
-    const actual = renderInfo('several colors', 'red, orange, green, blue.', 'colors');
-
-    it('has tagName: "DETAILS"', () => {
-      expect(actual).to.have.property('tagName', 'DETAILS');
-    });
-    it('has id: "colors"', () => {
-      expect(actual).to.have.property('id', 'colors');
-    });
-    it('has childElementCount: 2', () => {
-      expect(actual).to.have.property('childElementCount', 2);
-    });
-    console.dir(actual);
-
-    describe(`.children[0]`, () => {
-      const actualChild = actual.children[0];
-      it('has tagName: "SUMMARY"', () => {
-        expect(actualChild).to.have.property('tagName', 'SUMMARY');
-      });
-      it('has text: "several colors"', () => {
-        expect(actualChild).to.have.text('several colors');
-      });
-      it('has childElementCount: 0', () => {
-        expect(actualChild).to.have.property('childElementCount', 0);
-      });
-      console.dir(actualChild);
+        describe(`.children[1]`, () => {
+            const actualChild = actual.children[1];
+            it('has tagName: "P"', () => {
+                expect(actualChild).to.have.property('tagName', 'P');
+            });
+            it('has text: "red, orange, green, blue."', () => {
+                expect(actualChild).to.have.text('red, orange, green, blue.');
+            });
+            it('has childElementCount: 0', () => {
+                expect(actualChild).to.have.property('childElementCount', 0);
+            });
+            console.dir(actualChild);
+        });
     });
 
-    describe(`.children[1]`, () => {
-      const actualChild = actual.children[1];
-      it('has tagName: "P"', () => {
-        expect(actualChild).to.have.property('tagName', 'P');
-      });
-      it('has text: "red, orange, green, blue."', () => {
-        expect(actualChild).to.have.text('red, orange, green, blue.');
-      });
-      it('has childElementCount: 0', () => {
-        expect(actualChild).to.have.property('childElementCount', 0);
-      });
-      console.dir(actualChild);
-    });
-  });
+    describe('several colors, id "colors"', () => {
+        const actual = renderInfo('several colors', 'red, orange, green, blue.', 'colors');
 
-  describe('what is javascript?, id "js"', () => {
-    const actual = renderInfo('what is javascript?', 'a programming language!', 'js');
+        it('has tagName: "DETAILS"', () => {
+            expect(actual).to.have.property('tagName', 'DETAILS');
+        });
+        it('has id: "colors"', () => {
+            expect(actual).to.have.property('id', 'colors');
+        });
+        it('has childElementCount: 2', () => {
+            expect(actual).to.have.property('childElementCount', 2);
+        });
+        console.dir(actual);
 
-    it('has tagName: "DETAILS"', () => {
-      expect(actual).to.have.property('tagName', 'DETAILS');
-    });
-    it('has id: "js"', () => {
-      expect(actual).to.have.property('id', 'js');
-    });
-    it('has childElementCount: 2', () => {
-      expect(actual).to.have.property('childElementCount', 2);
-    });
-    console.dir(actual);
+        describe(`.children[0]`, () => {
+            const actualChild = actual.children[0];
+            it('has tagName: "SUMMARY"', () => {
+                expect(actualChild).to.have.property('tagName', 'SUMMARY');
+            });
+            it('has text: "several colors"', () => {
+                expect(actualChild).to.have.text('several colors');
+            });
+            it('has childElementCount: 0', () => {
+                expect(actualChild).to.have.property('childElementCount', 0);
+            });
+            console.dir(actualChild);
+        });
 
-    describe(`.children[0]`, () => {
-      const actualChild = actual.children[0];
-      it('has tagName: "SUMMARY"', () => {
-        expect(actualChild).to.have.property('tagName', 'SUMMARY');
-      });
-      it('has text: "what is javascript?"', () => {
-        expect(actualChild).to.have.text('what is javascript?');
-      });
-      it('has childElementCount: 0', () => {
-        expect(actualChild).to.have.property('childElementCount', 0);
-      });
-      console.dir(actualChild);
+        describe(`.children[1]`, () => {
+            const actualChild = actual.children[1];
+            it('has tagName: "P"', () => {
+                expect(actualChild).to.have.property('tagName', 'P');
+            });
+            it('has text: "red, orange, green, blue."', () => {
+                expect(actualChild).to.have.text('red, orange, green, blue.');
+            });
+            it('has childElementCount: 0', () => {
+                expect(actualChild).to.have.property('childElementCount', 0);
+            });
+            console.dir(actualChild);
+        });
     });
 
-    describe(`.children[1]`, () => {
-      const actualChild = actual.children[1];
-      it('has tagName: "P"', () => {
-        expect(actualChild).to.have.property('tagName', 'P');
-      });
-      it('has text: "a programming language!"', () => {
-        expect(actualChild).to.have.text('a programming language!');
-      });
-      it('has childElementCount: 0', () => {
-        expect(actualChild).to.have.property('childElementCount', 0);
-      });
-      console.dir(actualChild);
+    describe('what is javascript?, id "js"', () => {
+        const actual = renderInfo('what is javascript?', 'a programming language!', 'js');
+
+        it('has tagName: "DETAILS"', () => {
+            expect(actual).to.have.property('tagName', 'DETAILS');
+        });
+        it('has id: "js"', () => {
+            expect(actual).to.have.property('id', 'js');
+        });
+        it('has childElementCount: 2', () => {
+            expect(actual).to.have.property('childElementCount', 2);
+        });
+        console.dir(actual);
+
+        describe(`.children[0]`, () => {
+            const actualChild = actual.children[0];
+            it('has tagName: "SUMMARY"', () => {
+                expect(actualChild).to.have.property('tagName', 'SUMMARY');
+            });
+            it('has text: "what is javascript?"', () => {
+                expect(actualChild).to.have.text('what is javascript?');
+            });
+            it('has childElementCount: 0', () => {
+                expect(actualChild).to.have.property('childElementCount', 0);
+            });
+            console.dir(actualChild);
+        });
+
+        describe(`.children[1]`, () => {
+            const actualChild = actual.children[1];
+            it('has tagName: "P"', () => {
+                expect(actualChild).to.have.property('tagName', 'P');
+            });
+            it('has text: "a programming language!"', () => {
+                expect(actualChild).to.have.text('a programming language!');
+            });
+            it('has childElementCount: 0', () => {
+                expect(actualChild).to.have.property('childElementCount', 0);
+            });
+            console.dir(actualChild);
+        });
     });
-  });
 
 });
